2813c0e30849a67489a2615a74db324e85ca0fd6
owenwongny <efusionresearch@gmail.com>
2024-09-29 17:31:57 +0800

    Updates

---
+++ _next/static/8ZF_fdlodLrRHCcr2a0Vg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/510-c2e50adc1ef73cc5.js","static/chunks/pages/index-b6e3024a1ce69af0.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
